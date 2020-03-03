9ea4ebc1385f76eeb069e0c5aad6f5d978ef0d22
Heather Shirely <58097945+heethar@users.noreply.github.com>
2020-03-02 21:28:53 -0600

    Update weather-pull.sh

--- weather-pull.sh
+++ weather-pull.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 while [ 1 ]
 do
+		#Creates a temp file to curl wttr in
 		touch ~/.local/conky_configs/tmp/moon.tmp ~/.local/conky_configs/tmp/weather.tmp ~/.local/conky_configs/tmp/sun.tmp
 		#weather data
 		curl "http://wttr.in/?1ATF" --silent --max-time 3 > ~/.local/conky_configs/tmp/weather.tmp
