05e9a4bcec00421f26a9157149abe03a5d444241
Heather Shirely <58097945+heethar@users.noreply.github.com>
2020-03-03 23:29:20 -0600

    Update weather-pull.sh

--- weather-pull.sh
+++ weather-pull.sh
@@ -3,9 +3,11 @@ while [ 1 ]
 do
 		#Creates a temp file to curl wttr in, although, you do need to make sure these directories exist. 
 		touch ~/.local/conky_configs/tmp/moon.tmp ~/.local/conky_configs/tmp/weather.tmp 
-		#weather data
-		curl "http://wttr.in/?1ATF" --silent --max-time 3 > ~/.local/conky_configs/tmp/weather.tmp
-		#moon data
-		curl "http://wttr.in/moon?Q0ATF" --silent --max-time 3 > ~/.local/conky_configs/tmp/moon.tmp
+		#weather data, will make sure it updates every ten minutes
+		curl "http://wttr.in/?1ATF" --silent --max-time 3 > ~/.local/conky_configs/tmp/weather.tmp || echo " " > ~/.local/conky_configs/tmp/weather.tmp
+		#moon data, will remind you that you need some networking
+		curl "http://wttr.in/moon?Q0ATF" --silent --max-time 3 > ~/.local/conky_configs/tmp/moon.tmp || echo "Start the network..." > ~/.local/conky_configs/tmp/moon.tmp
+		cat ~/.local/conky_configs/tmp/{moon, weather}.tmp
+		#refresh every ten or so minutes.
 		sleep 600
 done
