91da92f04819248ae7f47fc910bf559fe0283e89
Heather Shirely <58097945+heethar@users.noreply.github.com>
2022-03-22 19:33:55 -0400

    Less Sloppy
    Directory destinations changed to match Arch documentation and clean up utter slop.

--- start-conky.sh
+++ start-conky.sh
@@ -1,12 +1,9 @@
 #/bin/bash
-~/.local/conky_configs/weather-pull.sh &
+~/.config/conky/weather-pull.sh &
 
 #wait 3 seconds
 sleep 3
 
 #start Conky with said conky config destinations (or, your own, I'm not your mother)
-#######################################################################
-#MAKE SURE YOUR FILE NAMES AND DIRECTORIES MATCH UP WITH EVERYTHING!!!#
-#######################################################################
-conky -c ~/.local/conky_configs/conky.conf &
-conky -c ~/.local/conky_configs/conkyrc-weather &
+conky -c ~/.config/conky/conky.conf &
+conky -c ~/.config/conky/conkyrc-weather &
