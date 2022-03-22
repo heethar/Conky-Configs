7e9887da8ff15c902fa5fd376f0bcd39278d6301
Heather Shirely <58097945+heethar@users.noreply.github.com>
2022-03-22 19:32:15 -0400

    Fixed Sloppiness
    Changed file directories to match the Arch documentation, and clean up utter slop.

--- weather-pull.sh
+++ weather-pull.sh
@@ -2,12 +2,15 @@
 while [ 1 ]
 do
 		#Creates a temp file to curl wttr in, although, you do need to make sure these directories exist. 
-		touch ~/.local/conky_configs/tmp/moon.tmp ~/.local/conky_configs/tmp/weather.tmp 
+		touch ~/.config/conky/tmp/moon.tmp ~/.config/conky/tmp/weather.tmp 
+
 		#weather data, will make sure it updates every ten minutes
-		curl "http://wttr.in/?1ATF" --silent --max-time 3 > ~/.local/conky_configs/tmp/weather.tmp || echo " " > ~/.local/conky_configs/tmp/weather.tmp
+		curl "http://wttr.in/?1ATF" --silent --max-time 3 > ~/.config/conky/tmp/weather.tmp || echo " " > ~/.config/conky/tmp/weather.tmp
+        
 		#moon data, will remind you that you need some networking
-		curl "http://wttr.in/moon?Q0ATF" --silent --max-time 3 > ~/.local/conky_configs/tmp/moon.tmp || echo "Start the network..." > ~/.local/conky_configs/tmp/moon.tmp
-		cat ~/.local/conky_configs/tmp/{moon, weather}.tmp
+		curl "http://wttr.in/moon?Q0ATF" --silent --max-time 3 > ~/.config/conky/tmp/moon.tmp || echo "Start the network..." > ~/.config/conky/tmp/moon.tmp
+		cat ~/.config/conky/tmp/{moon, weather}.tmp
+
 		#refresh every ten or so minutes.
 		sleep 600
 done
